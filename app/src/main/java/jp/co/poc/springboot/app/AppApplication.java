426a2bed51a93ef26061c7471290b7062b5ab783
suzukiti <h40396suzujun@gmail.com>
2023-06-27 08:23:43 +0900

    サンプルプロジェクト作成

---
+++ app/src/main/java/jp/co/poc/springboot/app/AppApplication.java
@@ -0,0 +1,13 @@
+package jp.co.poc.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
